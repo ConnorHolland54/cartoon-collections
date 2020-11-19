9934d532a8aa0dce323ff2aaf5ba9f3e33421d7e
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-19 14:42:40 -0700

    commit tree 8f6af06ecbf1a7e57913598a5b62a71a4e1cae75

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,59 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  i = 0
+  n = 1
+  while i < dwarves.length
+    puts("#{n}. #{dwarves[i]}")
+    i += 1
+    n += 1
+    end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  return planeteer_calls.map {|n| n.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  results = []
+  planeteer_calls.each do |x|
+    if x.length > 4
+      results << true
+    else
+      results << false
+    end
+  end
+  if results.include?(true)
+    return true
+  else 
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  results = []
+  i = 0
+
+  while i < array.length
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    if cheese_types.include?(array[i])
+      results << array[i]
+    end
+    i += 1
+  end
+
+  if results.length > 0
+    return results.join(" ")
+  else
+    return nil
+  end
+
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+array = []
+roll_call_dwarves(array)
+
+summon_captain_planet(array)
